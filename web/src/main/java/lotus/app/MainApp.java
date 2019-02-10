24f9c350ebbe24818825e7f4ae19ae616d80f4f3
lotusmeanseight <lotusmeansflower@tutanota.com>
2019-02-10 18:14:19 +0000

    basic stuff

--- web/src/main/java/lotus/app/MainApp.java
+++ web/src/main/java/lotus/app/MainApp.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
-@SpringBootApplication
+@EnableConfigServer
 @EnableNeo4jRepositories(basePackages = {"lotus.entity"})
+@SpringBootApplication
 public class MainApp {
 
     private static final Logger LOGGER = LogManager.getLogger();
