4872f23a9cafa16460c4bb8041a05ec1ce372c2f
Georg Göring <43638340+lotusmeanseight@users.noreply.github.com>
2019-02-14 11:31:02 +0000

    added two modules one for eureka (discovery) and one for zuul (gateway) moved some dependencies into the source where they are used. need to take a look at some of the dependencies again, especially the cloud starters.

---
+++ discovery/src/main/java/lotus/discovery/DiscoveryApp.java
@@ -0,0 +1,13 @@
+package lotus.discovery;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryApp {
+
+    public static void main(String[] args){
+
+    }
+
+
+}
