7cdeca1908b3e94748c3be6dc3d50844413c3370
lotusmeanseight <lotusmeansflower@tutanota.com>
2019-02-14 09:08:54 +0000

    not sure if content of application.yml is better moved into bootstrap yml preparation to use eureka discovery client moved dockerfiles into central directory

--- config/src/main/java/lotus/config/ConfigApp.java
+++ config/src/main/java/lotus/config/ConfigApp.java
@@ -4,10 +4,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigApp {
 
     private static final Logger LOGGER = LogManager.getLogger();
